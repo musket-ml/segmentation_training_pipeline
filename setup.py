98f15dd085765ff68a3e6b370ef2660bcd298551
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2018-10-28 17:09:47 +0700

    small fix to only include experiments that actually has some metrics in the report.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='segmentation_pipeline',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Petrochenko Pavel',
+      author_email='petrochenko.pavel.a@gmail.com',
+      license='MIT',
+      packages=['segmentation_pipeline'],
+      zip_safe=False)
