bffedc1452a6a6b0cbb96624a43d3df5cfe0a546
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2018-12-10 15:11:24 +0700

    publishing 0.30

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='segmentation_pipeline',
-      version='0.29',
+      version='0.30',
       description='Declaraqtively configured pipeline for image segmentation',
       url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
       author='Petrochenko Pavel',
@@ -10,5 +10,5 @@ setup(name='segmentation_pipeline',
       packages=setuptools.find_packages(),
       include_package_data=True,
       dependency_links=['https://github.com/aleju/imgaug'],
-      install_requires=["musket_core>=0.28"],
+      install_requires=["musket_core>=0.30"],
       zip_safe=False)
