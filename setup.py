e3edef2afd15a29807386abc1fcfbebd3caff3e8
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-10-23 19:57:46 +0700

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='segmentation_pipeline',
-      version='0.41',
+      version='0.42',
       description='Declaraqtively configured pipeline for image segmentation',
       url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
       author='Petrochenko Pavel',
