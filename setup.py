6838ec117cb65e555de2ce56645314f85f5bc6f2
Denis Denisenko <denis.munch@gmail.com>
2019-11-18 08:13:15 +0700

    Adding readme.md contents as long description for packages.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import io
+import os
+import sys
+
 from setuptools import setup
 import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
 setup(name='segmentation_pipeline',
       version='0.431',
-      description='Declaraqtively configured pipeline for image segmentation',
-      url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
+      description='Segmentation support piepeline for Musket ML',
+      long_description=long_description,
+      url='https://github.com/musket-ml/segmentation_training_pipeline',
       author='Petrochenko Pavel',
       author_email='petrochenko.pavel.a@gmail.com',
       license='MIT',
